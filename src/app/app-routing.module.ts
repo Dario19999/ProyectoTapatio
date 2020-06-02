70340635335efd112797bcefa681231b2907bd67
Dario Menendez <dariomego@gmail.com>
2020-06-02 15:37:27 -0500

    Primer commit
    -rutas creadas
    -paginas creadas
    -inicio(incompleto)
    -nosotros(incompleto)

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { PublicacionesComponent } from './components/publicaciones/publicaciones.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'eventos', component: EventosComponent },
+  { path: 'publicaciones', component: PublicacionesComponent },
+  { path: 'galeria', component: GaleriaComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'inicio' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { PublicacionesComponent } from './components/publicaciones/publicaciones.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    InicioComponent,
+    EventosComponent,
+    PublicacionesComponent,
+    GaleriaComponent,
+    NosotrosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
